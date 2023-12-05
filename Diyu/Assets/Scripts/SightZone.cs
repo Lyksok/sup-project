986212eba97fa4e65359d31dce5bfefad53ea785
leon <leon.corbin-chatel@epita.fr>
2023-12-05 21:44:35 +0100

    build-dazz1

---
+++ Diyu/Assets/Scripts/SightZone.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+public class SightZone : MonoBehaviour
+{
+    //i feel like all of this is pretty explicit
+    public event Action<GameObject> onEnter = null;
+    public event Action<GameObject> onStay = null;
+    public event Action<GameObject> onExit = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        onEnter?.Invoke(other.gameObject);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        playercontroller pc = other.GetComponent<playercontroller>();
+            if (pc != null)
+            onStay?.Invoke(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        onExit?.Invoke(other.gameObject);
+    }
+}
