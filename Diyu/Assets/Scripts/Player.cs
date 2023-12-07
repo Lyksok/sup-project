d96d993fe209a43e849950435ef9737544cafcd5
Tosky <jans.guillope@gmail.com>
2023-12-07 13:40:24 +0100

    Player moves and rotate on TargetRay

--- Diyu/Assets/Scripts/Player.cs
+++ Diyu/Assets/Scripts/Player.cs
@@ -5,51 +5,51 @@ using Mirror;
 
 public class Player : NetworkBehaviour
 {
-    [SerializeField] private Transform playerBody = null;
+    [SerializeField] private Transform playerBody;
     [SerializeField] private LayerMask groundMask;
-    [SerializeField] private CharacterController characterController = null;
+    [SerializeField] private Rigidbody rigidBody;
     [SerializeField] private Camera playerCamera;
+    [SerializeField] private Transform targetRay;
+
     [SerializeField] private float movementSpeed = 5.0f;
 
-    [SerializeField] private Vector3 cameraOffset = new Vector3(0f, 2f, -5f);
+    private Vector3 initalOffset;
+
+    private Vector3 cameraPosition;
+
+    //[SerializeField] private Vector3 cameraOffset = new Vector3(0f, 2f, -5f);
 
     private void Start()
     {
         playerCamera = GetComponentInChildren<Camera>();
-        characterController = GetComponentInChildren<CharacterController>();
+
+        initalOffset = transform.position - playerBody.position;
     }
 
     void HandleMovement()
     {
-        if (isLocalPlayer)
-        {
-            float z = Input.GetAxis("Horizontal");
-            float x = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Vertical");
+        float z = Input.GetAxis("Horizontal");
 
-            // Utilisez seulement les axes horizontaux (x et z) pour le mouvement
-            Vector3 moveBy = new Vector3(x, 0f, -z);
+        //we normalize movements by dircube so player can rotate without the "forward" changing
+        Vector3 moveBy = transform.forward * x + transform.right * z;
 
-            // Normalize seulement les composantes horizontales
-            moveBy.Normalize();
-
-            // Transforme le vecteur de mouvement de l'espace local du joueur à l'espace mondial
-            moveBy = transform.TransformDirection(moveBy);
-
-            // Applique le mouvement au CharacterController
-            characterController.Move(moveBy * movementSpeed * Time.deltaTime);
-        }
+        rigidBody.MovePosition(transform.position + moveBy.normalized * movementSpeed * Time.fixedDeltaTime);
     }
 
     private (bool success, Vector3 position) GetMousePosition()
     {
+        //this ray gives mouse position 
         var ray = playerCamera.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, groundMask))
         {
+            //the Raycast hit something, return with the position.
             return (success: true, position: hitInfo.point);
         }
         else
         {
+            //the Raycast did not hit anything.
             return (success: false, position: Vector3.zero);
         }
     }
@@ -59,26 +59,38 @@ public class Player : NetworkBehaviour
         var (success, position) = GetMousePosition();
         if (success)
         {
-            var direction = position - playerBody.position;
+            //calculate the direction
+            var direction = position - transform.position;
+
             direction.y = 0;
-            playerBody.forward = direction;
+
+            //cake the transform of player look in the direction.
+            targetRay.forward = direction;
         }
     }
 
     void UpdateCameraPosition()
     {
-        // Appliquer l'offset de la caméra par rapport au joueur sans changer sa rotation
-        playerCamera.transform.position = playerBody.position + cameraOffset;
+        //makes cam follow target
+        cameraPosition = playerBody.position + initalOffset;
+        transform.position = cameraPosition;
+    }
+
+    void DrawRays()
+    {
+        //this draws ray from the front
+        var ray = new Ray(playerCamera.transform.position, targetRay.transform.forward);
+        Debug.DrawRay(playerCamera.transform.position, targetRay.transform.forward * 100.0f, Color.red);
     }
 
     void Update()
     {
-        HandleMovement();
-        Aim();
-
         if (isLocalPlayer)
         {
+            HandleMovement();
+            Aim();
             UpdateCameraPosition();
+            DrawRays();
         }
     }
 }
