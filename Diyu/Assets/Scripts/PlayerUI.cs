4ec4c800f83b8f92a2df59e7bb72e707793e583b
Tosky <jans.guillope@gmail.com>
2024-01-18 02:31:48 +0100

    Add nameText field to PlayerUI prefab

--- Diyu/Assets/Scripts/PlayerUI.cs
+++ Diyu/Assets/Scripts/PlayerUI.cs
@@ -1,9 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerUI : MonoBehaviour
 {
+    /*
+    This class is responsible for managing the player UI.
+    Its purpose is to save the player name and id waiting for the game to start.
+    */
+    // Initialize playerUI serial fields
+    [SerializeField] private TMP_Text nameText;
+
+    // Initialize playerUI variables
     private string _name;
     public string Name { get; private set; }
 
@@ -19,19 +26,16 @@ public class PlayerUI : MonoBehaviour
         _name = name;
     }
 
+    // Method to set player id
+    public void SetId(int id)
+    {
+        _playerId = id;
+        Debug.Log("Player id : " + _playerId);
+    }
+
     // Method to set team id
     public void SetTeamId(int id)
     {
         _teamId = id;
-    }
-
-    // Class constructor
-    public PlayerUI(string name, int id)
-    {
-        _name = name;
-        _playerId = id;
-
-        // Set canvas text to player name
-        GameObject.Find("NameUI").GetComponent<UnityEngine.UI.Text>().text = _name;
     }
 }
