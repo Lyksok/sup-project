f586d87e1e1496377b01b0da9fdb86caf29637d4
lykso <jans.guillope@gmail.com>
2024-05-21 19:50:12 +0200

    All fixes for site

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+async function injectHTML(filePath) {
+    
+    const response = await fetch(filePath)
+    .then(response => response.text())
+    .then(text => document.querySelector(".main").innerHTML = text)
+}
+
+injectHTML("./pages/accueil.html");
